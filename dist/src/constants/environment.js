b2cdd50ed95c084c1685a56279f28414dca0b8ba
mibaloch <157287033+mibaloch@users.noreply.github.com>
2024-02-14 11:33:25 +0500

    rimraf and pre-commit changes removed

--- dist/src/constants/environment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var env;
-(function (env) {
-    env.WHITE_LIST = ["http://localhost:3000", "http://localhost:3000/"];
-})(env || (env = {}));
-exports.default = env;
--- dist/src/middlewares/validate-authorization.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateAuthorization = void 0;
-const validateAuthorization = (ctx, next) => {
-    next();
-};
-exports.validateAuthorization = validateAuthorization;
